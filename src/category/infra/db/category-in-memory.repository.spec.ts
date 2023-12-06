d3f090d15ea717191babb6829811f47fd2c099a9
Fontinelle <epitaciofontinelle@live.com>
2023-12-06 20:13:06 +0000

    add in-memory repository with search for category

--- src/category/infra/db/category-in-memory.repository.spec.ts
+++ src/category/infra/db/category-in-memory.repository.spec.ts
@@ -1,13 +1,80 @@
 import { CategoryInMemoryRepository } from './category-in-memory.repository';
-import { Uuid } from '../../../shared/domain/value-objects/uuid.vo';
 import { Category } from '../../domain/category.entity';
 
 describe('CategoryInMemoryRepository', () => {
-  it('should return the Category class when calling getEntity()', async () => {
-    const repository = new CategoryInMemoryRepository();
+  let repository: CategoryInMemoryRepository;
 
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+  });
+
+  it('should return the Category class when calling getEntity()', async () => {
     const getEntity = await repository.getEntity();
 
     expect(getEntity).toEqual(Category);
+  });
+
+  it('should filter items when filter object is null', async () => {
+    const items = [
+      new Category({ name: 'test' }),
+      new Category({ name: 'TEST' }),
+      new Category({ name: 'fake' }),
+    ];
+
+    const filterSpy = jest.spyOn(items, 'filter');
+
+    const itemsFiltered = await repository['applyFilter'](items, null);
+    expect(filterSpy).not.toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual(items);
+  });
+
+  it('should filter items using filter parameter', async () => {
+    const items = [
+      new Category({ name: 'test' }),
+      new Category({ name: 'TEST' }),
+      new Category({ name: 'fake' }),
+    ];
+
+    const filterSpy = jest.spyOn(items, 'filter');
+
+    const itemsFiltered = await repository['applyFilter'](items, 'TEST');
+    expect(filterSpy).toHaveBeenCalledTimes(1);
+    expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+  });
+
+  it('should sort items by createdAt desc when sort is null', async () => {
+    const createdAt = new Date();
+
+    const items = [
+      new Category({ name: 'test', createdAt }),
+      new Category({
+        name: 'TEST',
+        createdAt: new Date(createdAt.getTime() + 100),
+      }),
+      new Category({
+        name: 'fake',
+        createdAt: new Date(createdAt.getTime() + 200),
+      }),
+    ];
+
+    const itemsSorted = await repository['applySort'](items, null, null);
+
+    expect(itemsSorted).toStrictEqual(items.reverse());
+  });
+
+  it('should sort items by name', async () => {
+    const items = [
+      new Category({ name: 'a' }),
+      new Category({ name: 'b' }),
+      new Category({ name: 'c' }),
+    ];
+
+    let itemsSorted = await repository['applySort'](items, 'name', 'asc');
+
+    expect(itemsSorted).toStrictEqual(items);
+
+    itemsSorted = await repository['applySort'](items, 'name', 'desc');
+
+    expect(itemsSorted).toStrictEqual(items.reverse());
   });
 });
--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,12 +1,36 @@
+import { SortDirection } from '../../../shared/domain/repository/search-params';
 import { Uuid } from '../../../shared/domain/value-objects/uuid.vo';
-import { InMemoryRepository } from '../../../shared/infra/db/in-memory/in-memory.repository';
+import { InMemorySearchableRepository } from '../../../shared/infra/db/in-memory/in-memory-searchable.repository';
 import { Category } from '../../domain/category.entity';
 
-export class CategoryInMemoryRepository extends InMemoryRepository<
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<
   Category,
   Uuid
 > {
+  sortableFields: string[] = ['createdAt', 'name'];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string,
+  ): Promise<Category[]> {
+    if (!filter) return items;
+
+    return items.filter(item =>
+      item.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }
+
   getEntity(): new (...args: any[]) => Category {
     return Category;
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ) {
+    return sort
+      ? super.applySort(items, sort, sortDir)
+      : super.applySort(items, 'createdAt', 'desc');
   }
 }
